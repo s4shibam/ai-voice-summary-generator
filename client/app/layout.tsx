07266a0824d91da65250904988c1f1d380f25cc7
s4shibam <s4shibam@gmail.com>
2026-02-25 13:51:20 +0530

    feat: initialize client application with Next.js, TypeScript, and Tailwind CSS setup

---
+++ client/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin']
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+});
+
+export const metadata: Metadata = {
+  title: 'AI Voice Summary Generator',
+  description:
+    'A simple web application that allows you to record your voice and get a summary of what you said.'
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
